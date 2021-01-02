63370c37579e2df547f254dd9bc129ad364ed085
Yosuke Kagisaki <kagisaki@gmail.com>
2021-01-02 21:16:19 +0900

    adding DTO

---
+++ src/main/java/api/xxx/net/restapi/entities/CompletedStock.java
@@ -0,0 +1,32 @@
+package api.xxx.net.restapi.entities;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+
+import javax.persistence.Entity;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
+
+@Entity
+@Table("CompletedStock")
+public class CompletedStock {
+    @Id
+    BigDecimal id;
+
+    @Column("ItemKey")
+    BigDecimal itemKey;
+
+    @Column("Quantity")
+    BigDecimal quantity;
+
+    @Column("Location")
+    String location;
+
+    @Column("CreatedDate")
+    Timestamp createdDate;
+
+    @Column("ModifiedDate")
+    Timestamp modifiedDate;
+
+}
