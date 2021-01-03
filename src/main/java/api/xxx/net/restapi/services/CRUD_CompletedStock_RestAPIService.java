fea0c9c2d0f11d286f2d2bb042adfdad11e86813
Yosuke Kagisaki <kagisaki@gmail.com>
2021-01-03 11:44:13 +0900

    Adding restAPI Service

---
+++ src/main/java/api/xxx/net/restapi/services/CRUD_CompletedStock_RestAPIService.java
@@ -0,0 +1,5 @@
+package api.xxx.net.restapi.services;
+
+public class CRUD_CompletedStock_RestAPIService {
+    
+}
---
+++ src/main/java/api/xxx/net/restapi/services/Sample_Greeting_RestAPIService.java
@@ -0,0 +1,19 @@
+package api.xxx.net.restapi.services;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import api.xxx.net.restapi.entities.Greeting;
+
+@RestController
+public class Sample_Greeting_RestAPIService {
+
+    private static final String template = "Hello, %s!";
+
+    @GetMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return new Greeting(1234L, String.format(template, name));
+	}
+    
+}
