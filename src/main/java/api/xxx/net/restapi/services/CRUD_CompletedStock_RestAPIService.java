c1531be0b6775bd3856ef82608bacc0c9a1d2e9a
Yosuke Kagisaki <kagisaki@gmail.com>
2021-01-05 22:50:14 +0900

    adding post service

--- src/main/java/api/xxx/net/restapi/services/CRUD_CompletedStock_RestAPIService.java
+++ src/main/java/api/xxx/net/restapi/services/CRUD_CompletedStock_RestAPIService.java
@@ -3,8 +3,12 @@ package api.xxx.net.restapi.services;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import api.xxx.net.restapi.entities.CompletedStock;
@@ -26,5 +30,14 @@ public class CRUD_CompletedStock_RestAPIService {
         List<CompletedStock> resultList = new ArrayList<CompletedStock>();
         completedStockRepository.findAll().forEach(stock -> resultList.add(stock));
         return resultList;
+    }
+    /**
+     * Creating completed stock list without conflict. NOT COMPLETED! SECURITY Impl FIRST!
+     * @param completedStockList
+     */
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void  createCompletedStocks(@RequestBody List<CompletedStock> completedStockList) {
+        completedStockRepository.saveAll(completedStockList);
     }
 }
