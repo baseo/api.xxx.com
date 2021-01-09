095b7b1fe20bee2d3d5352ac732cdbfd94c72dc3
Yosuke Kagisaki <kagisaki@gmail.com>
2021-01-10 07:58:04 +0900

    adding confings

---
+++ src/main/java/api/xxx/net/restapi/entities/User.java
@@ -0,0 +1,42 @@
+package api.xxx.net.restapi.entities;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+
+import javax.persistence.Entity;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table("User")
+public class User {
+    @Id
+    @Column("Id")
+    BigDecimal id;
+
+    @Column("FirstName")
+    String firstName;
+
+    @Column("LastName")
+    String lastName;
+
+    @Column("Email")
+    String email;
+
+    @Column("Password")
+    String password;
+
+    @Column("CreatedDate")
+    Timestamp createdDate;
+
+    @Column("ModifiedDate")
+    Timestamp modifiedDate;
+
+    @Column("Isdeleted")
+    boolean isDelted;
+}
---
+++ src/main/java/api/xxx/net/restapi/repositries/UserRepository.java
@@ -0,0 +1,9 @@
+package api.xxx.net.restapi.repositries;
+
+import org.springframework.data.repository.CrudRepository;
+
+import api.xxx.net.restapi.entities.User;
+
+public interface UserRepository extends CrudRepository<User, Long>{
+     
+}
