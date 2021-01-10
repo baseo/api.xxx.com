f9a2895cd932cedca40ec17603312af03ee3c559
Yosuke Kagisaki <kagisaki@gmail.com>
2021-01-10 20:48:29 +0900

    testing https config

--- src/main/java/api/xxx/net/restapi/Configurations/RestApiSecurityConfiguration.java
+++ src/main/java/api/xxx/net/restapi/Configurations/RestApiSecurityConfiguration.java
@@ -1,10 +1,17 @@
 package api.xxx.net.restapi.Configurations;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
-public class RestApiSecurityConfiguration {
+public class RestApiSecurityConfiguration extends WebSecurityConfigurerAdapter{
     
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        //http.authorizeRequests()
+        //        .
+    }
 }
