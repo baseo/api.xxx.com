05e9d858c0d308f9b741bb04962253a44e7e7067
Yosuke Kagisaki <kagisaki@gmail.com>
2021-01-18 00:21:01 +0900

    adding H2 database for test in pom.xml

--- src/test/java/api/xxx/net/restapi/repositories/UserRepositoryTest.java
+++ src/test/java/api/xxx/net/restapi/repositories/UserRepositoryTest.java
@@ -1,20 +1,35 @@
 package api.xxx.net.restapi.repositories;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import api.xxx.net.restapi.entities.User;
 import api.xxx.net.restapi.repositries.UserRepository;
 
-@SpringBootTest
-@Configuration
+@RunWith(SpringRunner.class)
+@DataJdbcTest
 public class UserRepositoryTest{
+
+    @Autowired
+    private TestEntityManager manager;
+
     @Autowired
     private UserRepository userRepository;
     
     @Test
     public void findByEmailTest() {
+        User testUser = new User();
+        testUser.setFirstName("Einstein");
+        testUser.setLastName("Astro");
+        testUser.setEmail("test@test.com");
+        testUser.setPassword("test");
+        
+        manager.persistAndFlush(testUser);
+
         String inspectedEmail = "test@test.com";
 
         userRepository.findByEmail(inspectedEmail);
