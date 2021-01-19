abb9ca25b170ff7fa747e898cf51e0e79a9b6580
Yosuke Kagisaki <kagisaki@gmail.com>
2021-01-20 00:07:06 +0900

    edit schema.sql

--- src/test/resouces/schema.sql
+++ src/test/resouces/schema.sql
@@ -1,1 +1,14 @@
-CREATE
+CREATE TABLE IF NOT EXISTS User
+(
+    "Id" bigint NOT NULL auto_increment,
+    "FirstName" VARCHAR(80) NOT NULL,
+    "LastName" VARCHAR(80) NOT NULL,
+    "FirstName" VARCHAR(80) NOT NULL,
+    "Email" VARCHAR(256) NOT NULL,
+    "Password" VARCHAR(256) NOT NULL,
+    "SecurityToken" VARCHAR(256) ,
+    "CreatedDate" NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "ModifiedDate" NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    "IsDeleted" boolean,
+    PRIMARY KEY(Id)
+)
